375e833a21d7ed69a7795d612d98cb0e5f774414
Vaidotak <pehtrauhk@gmail.com>
2022-06-09 22:03:58 +0300

    Antra paskaita

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,19 @@
 public class Main {
+
     public static void main(String[] args) {
-        for (int i = 0; i < args.length; i++) {
-            System.out.println(args[i]);
-        }
-        System.out.println("Hello world!");
+        int a = 10;
+        a++;
+        int b = 9;
+        b += 1;
+        float rez = a * b;
+        System.out.println(rez);
+        String tekstas = "pavarde";
+        System.out.println(tekstas.charAt(2));
+        System.out.println(tekstas.length());
+
+        String s = "Mano vardas Vaidotas";
+        System.out.println(s.substring(12));
+        System.out.println(s.substring(0, 4));
+
     }
 }
