d88601adb69a06c624ad3aac8de9ccac922ce475
Vaidotak <pehtrauhk@gmail.com>
2022-06-15 08:59:22 +0300

    Trečia paskaita. Figūrų konstruktorius

---
+++ src/Figura.java
@@ -0,0 +1,48 @@
+public class Figura {
+
+    private String pavadinimas;
+    private int plotis;
+    private int aukstis;
+
+    public Figura() {
+    }
+
+    public Figura(String pavadinimas, int plotis, int aukstis) {
+        this.pavadinimas = pavadinimas;
+        this.plotis = plotis;
+        this.aukstis = aukstis;
+    }
+
+    public String getPavadinimas() {
+        return pavadinimas;
+    }
+
+    public void setPavadinimas(String pavadinimas) {
+        this.pavadinimas = pavadinimas;
+    }
+
+    public int getPlotis() {
+        return plotis;
+    }
+
+    public void setPlotis(int plotis) {
+        this.plotis = plotis;
+    }
+
+    public int getAukstis() {
+        return aukstis;
+    }
+
+    public void setAukstis(int aukstis) {
+        this.aukstis = aukstis;
+    }
+
+    @Override
+    public String toString() {
+        return "Figura{" +
+                "pavadinimas='" + pavadinimas + '\'' +
+                ", plotis=" + plotis +
+                ", aukstis=" + aukstis +
+                '}';
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,33 @@
 public class Main {
 
     public static void main(String[] args) {
-        int a = 10;
+        int a = 1;
         a++;
-        int b = 9;
+        int b = 1;
         b += 1;
         float rez = a * b;
         System.out.println(rez);
         String tekstas = "pavarde";
         System.out.println(tekstas.charAt(2));
         System.out.println(tekstas.length());
+        System.out.println(tekstas);
 
         String s = "Mano vardas Vaidotas";
         System.out.println(s.substring(12));
         System.out.println(s.substring(0, 4));
 
+
+        Figura figura = new Figura();
+        figura.setPavadinimas("Kvadratas");
+        figura.setPlotis(20);
+        figura.setAukstis(30);
+        System.out.println(figura.toString());
+
+        Figura figura1 = new Figura();
+        figura1.setPavadinimas("Staciakampis");
+        figura1.setPlotis(50);
+        figura1.setAukstis(10);
+        System.out.println(figura1);
     }
+
 }
